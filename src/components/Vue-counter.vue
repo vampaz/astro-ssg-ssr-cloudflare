2046cc8c353470f881b3dea407c4b80d6284ea8f
vampaz <vampaz@gmail.com>
2022-11-29 21:56:27 +0100

    Adding vue component SSG example

---
+++ src/components/Vue-counter.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="vue-story">
+		<h1>{{ state.count }}</h1>
+		<button @click="increment">Increment</button>
+	</div>
+</template>
+
+<script setup>
+import { reactive, onBeforeMount } from 'vue'
+const state = reactive({ count: 1 })
+
+function increment() {
+	state.count++
+}
+</script>
